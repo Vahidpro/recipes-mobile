cde3793d303555ffa2e5173b5b3eb01021428378
Vahid Porki <ivahidp@gmail.com>
2023-01-26 12:29:33 +0330

    Diaplay images when tap category

---
+++ components/MealItem.js
@@ -0,0 +1,31 @@
+import { StyleSheet, View, Text, Pressable, Image } from "react-native";
+
+function mealItem({ title, imageUrl }) {
+	return (
+		<View style={styles.container}>
+			<Pressable>
+				<View>
+					<Image style={styles.image} source={{ uri: imageUrl }}></Image>
+					<Text style={styles.title}>{title}</Text>
+				</View>
+			</Pressable>
+		</View>
+	);
+}
+export default mealItem;
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+	},
+	image: {
+		width: "100%",
+		height: 200,
+		borderRadius: 25,
+	},
+	title: {
+		fontWeight: "bold",
+		textAlign: "center",
+		fontSize: 18,
+	},
+});
--- screens/MealsOverviwScreen.js
+++ screens/MealsOverviwScreen.js
@@ -1,10 +1,26 @@
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, View } from "react-native";
+import { MEALS } from "../data/dummy-data";
+import MealItem from "../components/MealItem";
 
 function MealsOverviewScreen({ route }) {
 	const catId = route.params.categoryId;
+
+	const displayedMeals = MEALS.filter((mealItem) => {
+		return mealItem.categoryIds.indexOf(catId) >= 0;
+	});
+
+	const renderMealItem = (itemData) => {
+		return (
+			<MealItem title={itemData.item.title} imageUrl={itemData.item.imageUrl} />
+		);
+	};
 	return (
 		<View style={styles.container}>
-			<Text>Meals Overview Screen {catId}</Text>
+			<FlatList
+				data={displayedMeals}
+				keyExtractor={(item) => item.id}
+				renderItem={renderMealItem}
+			></FlatList>
 		</View>
 	);
 }
