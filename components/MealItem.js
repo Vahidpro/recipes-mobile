3bc85bce6034f23e168f1371a8b48a82423d27fb
Vahid Porki <ivahidp@gmail.com>
2023-01-26 12:37:33 +0330

    Added extra information to MealItem

--- components/MealItem.js
+++ components/MealItem.js
@@ -1,12 +1,17 @@
 import { StyleSheet, View, Text, Pressable, Image } from "react-native";
 
-function mealItem({ title, imageUrl }) {
+function mealItem({ title, imageUrl, duration, complexity, affordability }) {
 	return (
 		<View style={styles.container}>
 			<Pressable>
 				<View>
 					<Image style={styles.image} source={{ uri: imageUrl }}></Image>
 					<Text style={styles.title}>{title}</Text>
+				</View>
+				<View>
+					<Text>{duration}m</Text>
+					<Text>{complexity.toUpperCase()}</Text>
+					<Text>{affordability.toUpperCase()}</Text>
 				</View>
 			</Pressable>
 		</View>
