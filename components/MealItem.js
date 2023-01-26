c946fd58873232cfcab2ad551574ae87585880c1
Vahid Porki <ivahidp@gmail.com>
2023-01-26 12:57:57 +0330

    Applied styling to details

--- components/MealItem.js
+++ components/MealItem.js
@@ -1,17 +1,30 @@
-import { StyleSheet, View, Text, Pressable, Image } from "react-native";
+import {
+	StyleSheet,
+	View,
+	Text,
+	Pressable,
+	Image,
+	Platform,
+} from "react-native";
 
 function mealItem({ title, imageUrl, duration, complexity, affordability }) {
 	return (
 		<View style={styles.container}>
-			<Pressable>
+			<Pressable
+				android_ripple={{ color: "#ccc" }}
+				style={({ pressed }) => [
+					styles.button,
+					pressed ? styles.buttonPressed : "null",
+				]}
+			>
 				<View>
 					<Image style={styles.image} source={{ uri: imageUrl }}></Image>
 					<Text style={styles.title}>{title}</Text>
 				</View>
-				<View>
-					<Text>{duration}m</Text>
-					<Text>{complexity.toUpperCase()}</Text>
-					<Text>{affordability.toUpperCase()}</Text>
+				<View style={styles.details}>
+					<Text style={styles.detailItem}>{duration}m</Text>
+					<Text style={styles.detailItem}>{complexity.toUpperCase()}</Text>
+					<Text style={styles.detailItem}>{affordability.toUpperCase()}</Text>
 				</View>
 			</Pressable>
 		</View>
@@ -21,16 +34,36 @@ export default mealItem;
 
 const styles = StyleSheet.create({
 	container: {
-		flex: 1,
+		margin: 16,
+		backgroundColor: "white",
+		borderRadius: 25,
+		overflow: "hidden",
+		elevation: 10,
+		shadowColor: "black",
+		shadowOpacity: 0.25,
+		shadowOffset: { width: 0, height: 2 },
+		overflow: Platform.OS === "android" ? "hidden" : "visible",
 	},
 	image: {
 		width: "100%",
 		height: 200,
-		borderRadius: 25,
+		borderRadius: 20,
 	},
 	title: {
 		fontWeight: "bold",
 		textAlign: "center",
 		fontSize: 18,
+		margin: 8,
 	},
+	details: {
+		flexDirection: "row",
+		justifyContent: "center",
+		alignItems: "center",
+		padding: 8,
+	},
+	detailItem: {
+		fontSize: 12,
+		marginHorizontal: 4,
+	},
+	buttonPressed: { opacity: 0.7 },
 });
